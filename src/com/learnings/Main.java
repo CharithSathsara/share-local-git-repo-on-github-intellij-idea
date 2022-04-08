200e13360236bf0d63ddba1898c4c44683326516
CharithSathsara <charithsathsara169@gmail.com>
2022-04-08 23:38:25 +0530

    Practice pushing changes to github

--- src/com/learnings/Main.java
+++ src/com/learnings/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println("Charith Sathsara");
         System.out.println("Computer Science");
 
+        System.out.println("Practice pushing changes to github");
 
     }
 }
