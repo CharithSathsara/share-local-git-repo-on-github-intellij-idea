2b98ded59314585a4cce0a1f0e4177ed22026749
CharithSathsara <charithsathsara169@gmail.com>
2022-04-08 22:53:55 +0530

    Initial Commit. Practice the basic stuff about Git.

---
+++ src/com/learnings/Main.java
@@ -0,0 +1,11 @@
+package com.learnings;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("We want to create local Git Repo For IntelliJ IDEA Project");
+        System.out.println("Then we save the progress each step of the way...");
+
+    }
+}
